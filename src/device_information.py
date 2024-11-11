fc608ecafda0a7b2480cc5c4b80fdcbb8b8ebd84
rynovanwyk <91536100+rynovanwyk@users.noreply.github.com>
2024-11-11 21:02:33 +0200

    Initial commit

--- src/device_information.py
+++ src/device_information.py
@@ -88,7 +88,8 @@ class DeviceInfo:
                     ValueError('Datatype must be either "uint" or "char bits"')
 
             return try_n(do_read, tries=4)
-
+    
+    # Here is where I should start
     def write_bits(self, strobes: List[TimeStrober], register: int):
         strobes = [
             s if not isinstance(s, TimeStrober) else s.get_value() for s in strobes
